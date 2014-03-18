e6d91838b67f5463483d9c5d47992ce83b1459c7
Drew Rygh <drew.rygh@gmail.com>
2014-03-18 16:52:18 -0700

    Initial commit.

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, send_from_directory
+from flask import render_template
+import json
+
+app = Flask(__name__, static_folder='static')
+
+
+@app.route('/robots.txt')
+@app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', name='index')
+
+
+@app.route('/blog/')
+def blog():
+    df = open('posts.json').read()
+    posts = json.loads(df)
+    return render_template('blog/blog.html', name='blog', posts=posts)
+
+
+@app.route('/blog/<title_slug>')
+def blog_post(title_slug):
+    df = open('posts.json').read()
+    posts = json.loads(df)
+    fp = [p for p in posts if p['title_slug'] == title_slug][0]
+    return render_template(fp['file'], post=p)
+
+if __name__ == '__main__':
+    app.run()
